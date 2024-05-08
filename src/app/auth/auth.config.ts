7c5085a2bad4e7dd9ad80809c47e3d5d9a6f3832
Madalina Rosca <madalina.rosca.bp@emeal.nttdata.com>
2024-05-08 11:57:09 +0300

    Updates for production mode

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -2,15 +2,16 @@ import {
   IPublicClientApplication,
   PublicClientApplication,
 } from '@azure/msal-browser';
+import { environment } from 'src/environments/environment';
 
 export const MS_GRAPH_URL = 'https://graph.microsoft.com/v1.0';
 
 export const msalConfig = {
   // scopeUrl: 'api://xxxxxxx-xxxx-xxxx-a247-6023a95xxxxx/api',
-  clientId: '8c4c164b-93be-4333-8518-615cd153646b',
-  tenantName: '3260ea4d-aa20-465a-a197-28afd38975aa',
+  clientId: environment.clientId,
+  tenantName: environment.tenantName,
+  appUrl: environment.appUrl,
   scopes: ['openid', 'profile', 'User.Read', 'Sites.FullControl.All'],
-  appUrl: 'http://localhost:4200',
   location: 'localStorage',
   graphUrl: MS_GRAPH_URL,
 };
