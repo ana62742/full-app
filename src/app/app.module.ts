5b1b41aa80aaed0e11616ab7867b0527709d222a
Madalina Rosca <madalina.rosca.bp@emeal.nttdata.com>
2024-05-15 13:21:15 +0300

    Fixed conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,15 +16,11 @@ import { InteractionType } from '@azure/msal-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-<<<<<<< HEAD
-import { DxButtonModule, DxPopupModule, DxTemplateModule, DxTextBoxModule } from 'devextreme-angular';
-=======
 import {
   DxButtonModule,
   DxTextBoxModule,
   DxTooltipModule,
 } from 'devextreme-angular';
->>>>>>> fc9419b643d8bbccdc708411389b64d91b00dea0
 import { SettingsModule } from './settings/settings.module';
 import { StatisticsModule } from './statistics/statistics.module';
 import {
@@ -47,10 +43,6 @@ import { HomeComponent } from './shared/components/home/home.component';
     StatisticsModule,
     DxButtonModule,
     DxTextBoxModule,
-<<<<<<< HEAD
-    DxPopupModule,
-    DxTemplateModule
-=======
     DxTooltipModule,
     MsalModule.forRoot(
       MSALInsanceFactory(),
@@ -65,7 +57,6 @@ import { HomeComponent } from './shared/components/home/home.component';
         protectedResourceMap: new Map([[MS_GRAPH_URL, msalConfig.scopes]]),
       }
     ),
->>>>>>> fc9419b643d8bbccdc708411389b64d91b00dea0
   ],
   providers: [
     {
