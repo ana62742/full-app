2570fe87d540e8d196cda26aaf891bb0424c6cd2
Madalina Rosca <madalina.rosca.bp@emeal.nttdata.com>
2024-05-15 13:32:06 +0300

    Updates

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  clientId: '6dcd9c76-7530-4141-86ba-81677d5bc5ca',
-  tenantName: '485e1432-3aae-44d5-b543-6c0a97068711',
-  appUrl: 'http://localhost:4200',
-};
